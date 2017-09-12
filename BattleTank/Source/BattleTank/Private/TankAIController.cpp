ade294623b3f23094869b30324fea49bc6a237ee
Antonio <antonio.dorazio9@outlook.it>
2017-09-12 22:46:46 +0200

    PlayerController and AIController working

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "TankAIController.h"
+
+#include "Engine/World.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* PossessedTank = GetControlledTank();
+	if (PossessedTank != nullptr) {
+		FString LogMessage = PossessedTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("AI controller is possessing %s"), *LogMessage);
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("AI controller is possessing nothing"));
+	}
+
+	ATank* PlayerTank = GetPlayerTank();
+	if (PlayerTank != nullptr) {
+		FString LogMessage = PlayerTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("AI controller has %s as a target"), *LogMessage);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AI controller without target"));
+	}
+}
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APlayerController* TankPC = GetWorld()->GetFirstPlayerController();
+	if (TankPC != nullptr) {
+		return Cast<ATank>(TankPC->GetPawn());
+	}
+	else { return nullptr; }
+}
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* PossessedTank = GetControlledTank();
+	if (PossessedTank != nullptr) {
+		FString LogMessage = PossessedTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("Player controller is possessing %s"), *LogMessage);
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("Tank not possessed"));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
+
+
+
